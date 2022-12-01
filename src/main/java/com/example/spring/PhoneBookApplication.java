33d5fb34643b0a74ca7484e7f6d61bd47deb8977
Maksim Starodubtsev <maxstardub@mail.ru>
2022-12-01 20:11:03 +0300

    Deleted lib

--- src/main/java/com/example/spring/PhoneBookApplication.java
+++ src/main/java/com/example/spring/PhoneBookApplication.java
@@ -8,7 +8,6 @@ public class PhoneBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhoneBookApplication.class, args);
-
 	}
 
 }
--- src/test/java/com/example/spring/PhoneBookApplicationTests.java
+++ src/test/java/com/example/spring/PhoneBookApplicationTests.java
@@ -2,16 +2,21 @@ package com.example.spring;
 
 import com.example.spring.Model.Department;
 import com.example.spring.Service.DepartmentService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@RequiredArgsConstructor
+@Transactional
 class PhoneBookApplicationTests {
 
-	@Autowired
-	private DepartmentService departmentService;
+	private final DepartmentService departmentService;
 
 	@Test
 	void testAddingADepartment() {
