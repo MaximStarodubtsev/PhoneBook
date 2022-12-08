09e4750eac1098a623dd130ea72da08c21656e7e
Maksim Starodubtsev <maxstardub@mail.ru>
2022-12-08 21:59:54 +0300

    Changed filterChain method

--- src/main/java/com/example/spring/Configuration/SecurityConfiguration.java
+++ src/main/java/com/example/spring/Configuration/SecurityConfiguration.java
@@ -2,31 +2,29 @@ package com.example.spring.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration{
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    public void filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic(withDefaults())
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login")
                 .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true);
-
-        return http.build();
+                .invalidateHttpSession(true)
+                .and()
+                .httpBasic(Customizer.withDefaults());
     }
 
     @Bean
